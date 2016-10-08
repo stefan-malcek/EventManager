f95c4399a1d66684e1503e61992ef4e6d11fdb25
malcek <stefan.malcek1@gmail.com>
2016-10-08 19:51:42 +0200

    Add libraries

---
+++ Riganti.Utils.Infrastructure.EntityFramework/UserAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using BrockAllen.MembershipReboot.Relational;
+using Riganti.Utils.Infrastructure.Core;
+
+namespace Riganti.Utils.Infrastructure.EntityFramework
+{
+    /// <summary>
+    /// This is NOT official part of Riganti.Utils.Infrastructure
+    /// 
+    /// Base class for application user, 
+    /// all declared properties MUST be virtual
+    /// 
+    /// Please note this class needs to be referenced even from PL,
+    /// therefore its placed apart from other entities in DAL.
+    /// </summary>
+    public class UserAccount : RelationalUserAccount , IEntity<Guid>
+    {
+        //[Required]
+        [MaxLength(64)]
+        public virtual string FirstName { get; set; }
+
+        //[Required]
+        [MaxLength(64)]
+        public virtual string LastName { get; set; }
+
+        //[Required]
+        [MaxLength(1024)]
+        public virtual string Address { get; set; }
+
+        //[Required]
+        public virtual DateTime BirthDate { get; set; } = new DateTime(1980, 1, 1);
+    }
+}
