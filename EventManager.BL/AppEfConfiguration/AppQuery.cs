f4814e94ba96b5bfb14f29ff06f3a0638ee75bff
malcek <stefan.malcek1@gmail.com>
2016-10-31 23:44:04 +0100

    Add BL project structure

---
+++ EventManager.BL/AppEfConfiguration/AppQuery.cs
@@ -0,0 +1,13 @@
+﻿using EventManager.DAL;
+using Riganti.Utils.Infrastructure.Core;
+using Riganti.Utils.Infrastructure.EntityFramework;
+
+namespace EventManager.BL.AppEfConfiguration
+{
+    public abstract class AppQuery<T> : EntityFrameworkQuery<T>
+    {
+        public new EventManagerDbContext Context => (EventManagerDbContext)base.Context;
+
+        protected AppQuery(IUnitOfWorkProvider provider) : base(provider) { }
+    }
+}
---
+++ EventManager.BL/Bootstrap/AutoMapperConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+
+namespace EventManager.BL.Bootstrap
+{
+    public class AutoMapperConfig
+    {
+        public static void Initialize()
+        {
+            Mapper.Initialize(i =>
+            {
+                //TODO insert mapping configuration
+            });
+        }
+    }
+}
